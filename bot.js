784144636135e18a5826ac313254dabb5292475c
LittleWhole <wholelittle@gmail.com>
2016-12-31 21:46:32 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,8 @@
+var Discordie = require('discordie');
 
+const Events = Discordie.events
+const client = new Discordie();
+
+client.Dispatcher.on(Events.GATEWAY_READY, e => {
+    console.log('Connected as: ' + client.User.Username);
+});
