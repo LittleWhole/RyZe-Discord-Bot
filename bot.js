8eb150c8d2ea8aa1bc16fdf6582175f3353fd676
LittleWhole <wholelittle@gmail.com>
2017-01-01 14:35:24 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,22 @@
-var Discordie = require('discordie');
+var Discordie = require("discordie");
+var client = new Discordie();
 
-const Events = Discordie.events
-const client = new Discordie();
+var prefix = "@";
 
-client.Dispatcher.on(Events.GATEWAY_READY, e => {
-    console.log('Connected as: ' + client.User.Username);
+var found = true;
+
+client.connect({
+	token: "MjY0OTM1OTg0ODU0OTkwODQ5.C0rkSg.-ZaCg1qWKDD8VXCscPN6rF2L3V0"
+});
+
+// USE // FOR COMMENTS
+
+client.Dispatcher.on("GATEWAY_READY", e => {
+	console.log("Connected as: " + client.User.username);
+});
+
+client.Dispatcher.on("MESSAGE_CREATE", e => {
+	if (e.message.content. == "@ping") {
+    e.message.channel.sendMessage("Pong!");
+  }
 });
